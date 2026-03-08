d23c36f140416f682e61c2ab757fb27e9bcf83c5
Evgeniy Pivovarchik <eugene.pivovarchik@gmail.com>
2026-03-08 22:15:13 +0300

    add theory multilevel logic, add practice logic, update homeworks logic, add admin logic, little fixex

--- config/settings.py
+++ config/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
-
+# CSRF_TRUSTED_ORIGINS = []
 
 # Application definition
 
